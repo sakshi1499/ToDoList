237cb56f696f65b6f40be53feb359d8f5bf9d164
sakshi1499 <42520146+sakshi1499@users.noreply.github.com>
2019-05-25 03:01:37 +0530

    second commit

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.post("/",function(req,res){
   items.push(item);
 res.redirect("/");
 });
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("Server is running on port 3000");
 });
